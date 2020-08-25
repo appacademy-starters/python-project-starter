47260487c801c11c3dbe4adfb4e9fd8c31e81de9
Ian Bentley <ian@idbentley.com>
2020-08-25 17:01:40 -0400

    add csrf protection

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 const cors = require('cors');
 const createError = require('http-errors');
+const cookieParser = require('cookie-parser')
 const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
 const logger = require('morgan');
-
+const csurf = require('csurf');
 const routes = require('./routes');
 
 const app = express();
 
-app.use(cors({ origin: true }));
-app.use(helmet({ hsts: false }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cookieParser())
+
+
+// Security Middleware
+app.use(cors({ origin: true }));
+app.use(helmet({ hsts: false }));
+app.use(csurf({
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: false
+  }
+}));
+
+
 app.use(routes);
 
 // Serve React Application
+// This should come after routes, but before 404 and error handling.
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("/", (req, res) => {
--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import UserList from './components/UsersList.js'
+import UserList from './components/UsersList';
+
 
 function App() {
+
   return (
     <>
         <h1>User List: </h1>
