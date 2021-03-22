fb3c5803e42f99757987898450f80ed78e83abca
Ryan Webster <ryan.h.webster@gmail.com>
2021-03-22 10:30:57 -0700

    started bracket game

---
+++ app/models/teams.py
@@ -0,0 +1,17 @@
+from .db import db, col, flo, num, fk, pk, string, boo
+
+class Team(db.Model):
+    __tablename__="teams"
+
+    id = col(num, pk = True)
+    name  = col(string, nullable = False)
+    conference = col(num, fk("conferences.id"), nullable = False)
+    seed = col(num, nullable = False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "conference": self.conference,
+            "seed": self.seed
+        }
