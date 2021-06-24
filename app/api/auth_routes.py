4b38e7c3a53b07a8281e464969a2ca9c541cadb2
jshafto <jpshafto@gmail.com>
2021-06-24 18:19:33 -0400

    fix config to account for heroku+sqlalchemy1.4

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -34,7 +34,6 @@ def login():
     Logs a user in
     """
     form = LoginForm()
-    print(request.get_json())
     # Get the csrf_token from the request cookie and put it into the
     # form manually to validate_on_submit can be used
     form['csrf_token'].data = request.cookies['csrf_token']
