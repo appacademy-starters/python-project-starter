9e1261f80f2eeb4b70734ae42804bd04f1d967a5
mitchellreiss <mitchellreiss@gmail.com>
2021-07-28 13:23:09 -0700

    add 404 error handler
    Update the 404 handler and favicon route
    
    Remove static.json
    
    Add favicon link to index.html
    
    Fix href in favicon link
    
    Seperate favicon route from react_root route
    
    Update build
    
    Revert back to original react_root functionality
    
    Combine react_root with 404 handler
    
    Remove 404 handler
    
    Remove link tag for favicon

--- app/__init__.py
+++ app/__init__.py
@@ -73,10 +73,29 @@ def inject_csrf_token(response):
     return response
 
 
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(
+#         os.path.join(app.root_path, 'static'),
+#         'favicon.ico',
+#         mimetype='image/vnd.microsoft.icon'
+#     )
+
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def react_root(path):
     if path == 'favicon.ico':
-        # return app.send_static_file('favicon.ico')
-        return send_from_directory(app.static_folder, 'favicon.ico')
+        return send_from_directory(
+            app.static_folder, 'favicon.ico',
+            mimetype='image/vnd.microsoft.icon'
+        )
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    """
+    Revert 404 to index.html
+    """
     return send_from_directory(app.static_folder, 'index.html')
