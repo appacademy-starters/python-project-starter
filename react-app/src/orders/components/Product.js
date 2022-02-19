5ee41215fbc028796c74cdd802a37307ee1fbd48
Alexson Coelho <coelho@Alexsons-MacBook-Pro.local>
2022-02-19 10:37:23 -0500

    Product details route

--- react-app/src/orders/components/Product.js
+++ react-app/src/orders/components/Product.js
@@ -2,16 +2,16 @@ import React from "react";
 import { Card, Button, Col } from "react-bootstrap";
 import PropTypes from "prop-types";
 
-export const Product = ({ src, alt, title, description }) => {
+export const Product = ({ src, alt, title, description, handleClick }) => {
   return (
     <Col className='d-grid justify-content-around mb-2 mt-2'>
-      <Card className='text-center' style={{ width: "18rem" }}>
+      <Card className='text-center' style={{ width: "15rem" }}>
         <Card.Body className='m-4 rounded border border-dark'>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{description}</Card.Text>
           <img src={src} class='rounded mx-auto d-block' alt={alt} />
         </Card.Body>
-        <Button className='m-2' variant='dark'>
+        <Button className='m-2' variant='dark' onClick={handleClick}>
           Select
         </Button>
       </Card>
@@ -24,4 +24,5 @@ Product.propTypes = {
   alt: PropTypes.string,
   title: PropTypes.string,
   description: PropTypes.string,
+  handleClick: PropTypes.func.isRequired,
 };
