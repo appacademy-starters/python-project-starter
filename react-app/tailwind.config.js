cecf773613b73427e215114f9bce91b0253bc7e9
Alexson Coelho <coelho@Alexsons-MacBook-Pro-2.local>
2022-04-12 21:47:38 -0400

    update

--- react-app/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: [/Users/matthewcarpenter/Documents/CLIENTS/APPs/step-solution/python-project-starter/react-app/src],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
